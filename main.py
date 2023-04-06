ccdad15a88220be7375e3e895371bace0b99e95f
GIGACHAD-pixeldick <115863181+GIGACHAD-pixeldick@users.noreply.github.com>
2023-04-06 13:41:20 +0300

    add textures

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from settings import *
 from map import *
 from player import *
 from raycasting import *
+from object_render import *
 
 class Game:
     def __init__(self):
         pg.init()
+        pg.mouse.set_visible(False)
         self.screen = pg.display.set_mode(RESOLUTION)
         self.clock = pg.time.Clock()
         self.delta_time = 1
@@ -16,6 +18,7 @@ class Game:
         def new_game(self):
             self.map = Map(self)
             self.player = Player(self)
+            self.object_render = ObjectRender(self)
             self.raycasting = RayCasting(self)
 
         def update(self):
@@ -26,9 +29,10 @@ class Game:
             pg.display.set_caption(f'{self.clock.get_fps():.1f}')
 
         def draw(self):
-            self.screen.fill('black')
-            self.map.draw()
-            self.player.draw()
+            #self.screen.fill('black')
+            self.object_render.draw()
+            #self.map.draw()
+            #self.player.draw()
 
         def check_events(self):
             for event in pg.event.get():
