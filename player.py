88888385b67be2e2df132787967648496cc9b978
GIGACHAD-pixeldick <115863181+GIGACHAD-pixeldick@users.noreply.github.com>
2023-04-06 13:49:38 +0300

    fix problems

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ import math
 class Player:
     def __init__(self,game):
         self.game = game
-        self.x, self.y - PLAYER_POS
+        self.x, self.y = PLAYER_POS
         self.angle = PLAYER_ANGLE
 
     def movement(self):
