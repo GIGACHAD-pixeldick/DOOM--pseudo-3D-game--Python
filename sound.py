ca47d92e0b48d14308d0d9a9a2c6cea0a0a91347
GIGACHAD-pixeldick <115863181+GIGACHAD-pixeldick@users.noreply.github.com>
2023-04-09 12:39:28 +0300

    add sound for shotgun and animation

--- sound.py
+++ sound.py
@@ -2,4 +2,7 @@ import pygame as pg
 
 class Sound:
     def __init__(self, game):
-        pass
+        self.game = game
+        pg.mixer.init()
+        self.path = 'resources/sound/'
+        self.shotgun = pg.mixer.Sound(self.path + 'shotgun.wav')
